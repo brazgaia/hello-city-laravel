fe62896c8c5476db118cf4faec3c14c184b9ec56
DELAFOSSE STEPHANE <delafossestephane@macbook-pro-de-delafosse.home>
2021-04-03 15:20:49 +0200

    add about page and update links

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,12 +9,16 @@
 
     </head>
     <body>
-        <h1>Hello from Laleu !</h1>
 
-        <p>It's currently {{ date('h:i A')}}</p>
+        <h1>About Fr | Hello City</h1>
+
+        <p>Built with &hearts; by promessevidéo</p>
+
+        <p><a href="/">Revenir à la page d'accueil</p>
 
         <footer>
           <p>&copy; Promessevidéo {{ date('M Y')}}-Tous droits réservés &middot;<a href="/about-fr">About Fr</p>
         </footer>
+
     </body>
 </html>
